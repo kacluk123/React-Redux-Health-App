0af7e0261c233f65461dd33d9803a7346799d3bc
Kacper Łukasik <sakuy124@gmail.com>
2018-12-01 15:36:10 +0100

    Sharing added

--- src/components/usersdiet/OneUserDiet.js
+++ src/components/usersdiet/OneUserDiet.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {sendId} from "../../actions/IDActions";
+import {compose} from "redux"
+
+import {connect} from "react-redux";
 
 class OneUserDiet extends Component {
+    dietSend = (whole) =>{
+        this.props.sendId(whole)
+    }
     render() {
         return (
             <div style={{width: '200px', height: "240px",
                 backgroundColor: 'white', borderRadius: '50px', display: 'flex',
                 justifyContent: 'flex-start', flexDirection: 'column', margin: "10px", alignItems: 'center'}}>
-                <span>Name: {this.props.dietName}</span>
+                <span style={{paddingTop: '10px'}}>Name: {this.props.dietName}</span>
                 <span className="food-calories">Calories: {this.props.totalCalories}</span>
+                <span>{this.props.description.substr(0, 20)}...</span>
+
+                <span style={{fontSize: '15px'}}>Added by:</span>
+                <span>{this.props.profileName}</span>
+                <div style={{width: "50px", height: '50px', backgroundSize: 'cover',
+                    backgroundPosition:'center', backgroundImage: `url(${this.props.profileAvatar})`, borderRadius: '50%'}}></div>
+                <span className="food-calories">Likes: {this.props.likes.length > 0 ? this.props.likes.length : 0 }</span>
+                <button style={{fontSize: '14px'}} onClick={this.dietSend.bind(this, this.props.whole)} className="share">See more</button>
             </div>
         );
     }
@@ -16,4 +31,8 @@ class OneUserDiet extends Component {
 
 OneUserDiet.propTypes = {};
 
-export default OneUserDiet;
+export default compose(
+    connect((state)=>({diets: state.firestore.ordered.diets,
+        dietID: state.idRed,
+    }), { sendId }))(OneUserDiet);
+
--- src/components/usersdiet/usersdiet.js
+++ src/components/usersdiet/usersdiet.js
@@ -4,24 +4,37 @@ import {connect} from 'react-redux'
 import {compose} from "redux"
 import OneUserDiet from './OneUserDiet'
 import {firebaseConnect, firestoreConnect} from 'react-redux-firebase'
-class UsersDiets extends Component {
-    state = {
-        elo : 'eki',
-        obj : {},
-    }
-     componentDidMount(){
-         console.log(this.props.diets)
+import {sendId} from "../../actions/IDActions";
 
+class UsersDiets extends Component {
+    closeWindow =()=>{
+        this.props.sendId('')
     }
     render() {
-        const {diets} = this.props
+        console.log(this.props.diets)
+        const {diets, dietID} = this.props
+        console.log(dietID)
         return (
             <div className="container">
             <div className="card" style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+                {dietID.idDiet ? <div className="UserDietContainer">
+                    <i onClick={this.closeWindow} style={{alignSelf: 'flex-start',marginLeft: 'auto', cursor:'pointer', fontSize: '35px'}}className="fas fa-times"></i>
+
+                    <span className="user-diet-name">{dietID.idDiet.name}</span>
+                    <ol>{dietID.idDiet.arr.map(el=>
+                        <li style={{fontSize: '20px', color: "#393f4d"}}>{el.food}  <span className="food-calories">Calories: {el.calories}</span> </li>)}</ol>
+                    <p style={{fontSize: '20px', color: "#393f4d"}}>{dietID.idDiet.description}</p>
+
+
+                </div> : null}
                 {diets !== undefined ? diets.map(el=> <OneUserDiet key={el.id}
                                                                    dietName={el.name}
                                                                    totalCalories={el.totalCalories}
-
+                                                                   description={el.description}
+                                                                   profileName={el.profileName}
+                                                                   likes={el.likes}
+                                                                   profileAvatar={el.profileAvatar}
+                                                                   whole={el}
                 />) : []}
             </div>
             </div>
@@ -32,4 +45,6 @@ class UsersDiets extends Component {
 UsersDiets.propTypes = {};
 
 export default compose(firestoreConnect([{ collection: 'diets', }]),
-    connect((state)=>({diets: state.firestore.ordered.diets})))(UsersDiets);
+    connect((state)=>({diets: state.firestore.ordered.diets,
+                        dietID: state.idRed,
+    }), { sendId }))(UsersDiets);
