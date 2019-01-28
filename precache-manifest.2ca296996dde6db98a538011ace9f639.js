a85ef883b1429b2329dcd4d46903f811fbd8dfdc
Kacper Łukasik <sakuy124@gmail.com>
2019-01-28 21:46:09 +0100

    Updates

---
+++ precache-manifest.2ca296996dde6db98a538011ace9f639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Redux-Health-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bdc9804cf08bcb524b7ab299b6a41a08",
+    "url": "/React-Redux-Health-App/static/media/gym.bdc9804c.jpg"
+  },
+  {
+    "revision": "f93d441ae66b794049ca",
+    "url": "/React-Redux-Health-App/static/js/runtime~main.f93d441a.js"
+  },
+  {
+    "revision": "11a1938a0c972b982b3a",
+    "url": "/React-Redux-Health-App/static/js/main.11a1938a.chunk.js"
+  },
+  {
+    "revision": "e5713f8b2e905b654f50",
+    "url": "/React-Redux-Health-App/static/js/1.e5713f8b.chunk.js"
+  },
+  {
+    "revision": "11a1938a0c972b982b3a",
+    "url": "/React-Redux-Health-App/static/css/main.a80d3451.chunk.css"
+  },
+  {
+    "revision": "4c5bfd67490c279efd50263a846d3b47",
+    "url": "/React-Redux-Health-App/index.html"
+  }
+];
